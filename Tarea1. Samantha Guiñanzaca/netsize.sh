0097baf1bb86c6eac3f7d972a191c5b4e38b7a1b
SamanthaGuinanzaca <94935304+SamanthaGuinanzaca@users.noreply.github.com>
2021-12-09 14:58:28 -0500

    Update netsize.sh
    Escribo la instrucción del ejercicio 1.10.3 parte 1 de la Tarea 1

--- Tarea1. Samantha Guiñanzaca/netsize.sh
+++ Tarea1. Samantha Guiñanzaca/netsize.sh
@@ -1,1 +1,6 @@
 
+
+#Ejercicio 1.10.3
+##Parte 1
+
+## 1. Escriba un script que tome uno de estos archivos y determine el número de filas (polinizadores) y columnas (plantas). Tenga en cuenta que las columnas están separadas por espacios y que hay un espacio al final de cada línea.
