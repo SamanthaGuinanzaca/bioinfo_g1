f5074b01be65028363053bf22307ee6bc08a9bc2
SamanthaGuinanzaca <94935304+SamanthaGuinanzaca@users.noreply.github.com>
2021-12-10 19:28:42 -0500

    Se agrego comando para hallar filas de n50.txt

--- Tarea1. Samantha Guiñanzaca/netsize.sh
+++ Tarea1. Samantha Guiñanzaca/netsize.sh
@@ -1,6 +1,9 @@
 
 
-#Ejercicio 1.10.3
-##Parte 1
+echo 'Ejercicio 1.10.3'
 
-## 1. Escriba un script que tome uno de estos archivos y determine el número de filas (polinizadores) y columnas (plantas). Tenga en cuenta que las columnas están separadas por espacios y que hay un espacio al final de cada línea.
+echo 'Parte 1'
+
+echo '1. Escriba un script que tome uno de estos archivos y determine el número de filas (polinizadores) y columnas (plantas). Tenga en cuenta que las columnas están separadas por espacios y que hay un espacio al final de cada línea.'
+
+wc -l n50.txt
