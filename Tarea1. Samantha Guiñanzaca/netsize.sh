4d4cbce66265a10c37d4b99d958bf9d7707400c2
SamanthaGuinanzaca <94935304+SamanthaGuinanzaca@users.noreply.github.com>
2021-12-11 20:20:36 -0500

    Agregue código de echo y para hallar filas
    Agregue código de echo para "Numero de filas:" y conté las filas con el comando wc -l

--- Tarea1. Samantha Guiñanzaca/netsize.sh
+++ Tarea1. Samantha Guiñanzaca/netsize.sh
@@ -6,5 +6,6 @@ echo 'Parte 1'
 
 echo '1. Escriba un script que tome uno de estos archivos y determine el número de filas (polinizadores) y columnas (plantas). Tenga en cuenta que las columnas están separadas por espacios y que hay un espacio al final de cada línea.'
 
+ echo "Numero de filas:" ; wc -l n2.txt 
 
  
