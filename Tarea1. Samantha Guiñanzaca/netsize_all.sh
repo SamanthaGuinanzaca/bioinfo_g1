57652fc994b058e7b2eae51195f23a27509d0cb0
SamanthaGuinanzaca <94935304+SamanthaGuinanzaca@users.noreply.github.com>
2021-12-11 21:32:18 -0500

    Se agrego directorio
    Agregue el directorio para llegar al directorio Saavedra2013/

--- Tarea1. Samantha Guiñanzaca/netsize_all.sh
+++ Tarea1. Samantha Guiñanzaca/netsize_all.sh
@@ -2,4 +2,7 @@ Ejercicio 1.10.3
 
 Parte 2. Escriba un script que imprima el numero de filas y columnas para cada red. 
 
+cd Documents/Bioinformatica/CSB-master/unix/data/Saavedra2013
+
 wc -l *.txt 
+
