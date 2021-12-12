48401b9218f255a0f10d1631846d14f3b1dddb22
SamanthaGuinanzaca <94935304+SamanthaGuinanzaca@users.noreply.github.com>
2021-12-11 22:58:44 -0500

    Se elimino el siguiente código.
     for F in $Parte2
     do
      echo $F 
     done
    Se elimino este código ya que no es necesario tener este código ya que el siguiente bucle for hace la misma función.

--- Tarea1. Samantha Guiñanzaca/netsize_all.sh
+++ Tarea1. Samantha Guiñanzaca/netsize_all.sh
@@ -5,18 +5,13 @@ Parte 2. Escriba un script que imprima el numero de filas y columnas para cada r
 cd Documents/Bioinformatica/CSB-master/unix/data/Saavedra2013
 
 Parte2=../Saavedra2013/*.txt
-
- for F in $Parte2
- do
-  echo $F 
- done  
-
  for f in $Parte2
  do
     myrow=`cat $f | wc -l`
     mycol=`head -n 1 $f | tr -d ' ' | tr -d '\n' | wc -c`
     echo $f $myrow $mycol
 done 
+
 
 
 
