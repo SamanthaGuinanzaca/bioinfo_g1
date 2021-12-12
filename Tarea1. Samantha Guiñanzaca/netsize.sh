3c301654bbeb6468e02ed3161f2c7c692efb26e2
SamanthaGuinanzaca <94935304+SamanthaGuinanzaca@users.noreply.github.com>
2021-12-11 20:32:33 -0500

    Agregue el código anterior de filas para las columnas
    head -n 1 n2.txt | tr -d ' ' | tr -d '\n' | wc -c el codigo para hallar columnas se necesita más códigos para eliminar todos los espacios existentes para eso se utilizo tr- d  y tambien para borrar la linea del terminador tr -d '/n',  por ultimo  para contar el numero de columnas se utilizo wc -c

--- Tarea1. Samantha Guiñanzaca/netsize.sh
+++ Tarea1. Samantha Guiñanzaca/netsize.sh
@@ -6,6 +6,7 @@ echo 'Parte 1'
 
 echo '1. Escriba un script que tome uno de estos archivos y determine el número de filas (polinizadores) y columnas (plantas). Tenga en cuenta que las columnas están separadas por espacios y que hay un espacio al final de cada línea.'
 
- echo "Numero de filas:" ; wc -l n2.txt 
+echo "Numero de filas:" ; wc -l n2.txt 
 
+echo "Numero de columnas:" ; head -n 1 n2.txt | tr -d ' ' | tr -d '\n' | wc -c
  
