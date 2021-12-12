02d2868c91658b1c52c56cc76f9043502fe383ad
SamanthaGuinanzaca <94935304+SamanthaGuinanzaca@users.noreply.github.com>
2021-12-11 21:06:51 -0500

    Se unió los dos códigos
    Se unió los dos códigos

--- Tarea1. Samantha Guiñanzaca/netsize.sh
+++ Tarea1. Samantha Guiñanzaca/netsize.sh
@@ -6,8 +6,8 @@ echo 'Parte 1'
 
 echo '1. Escriba un script que tome uno de estos archivos y determine el número de filas (polinizadores) y columnas (plantas). Tenga en cuenta que las columnas están separadas por espacios y que hay un espacio al final de cada línea.'
 
-echo "Numero de filas:" ; wc -l n50.txt 
+echo "Numero de filas:" ; wc -l n50.txt echo "Numero de columnas:" ; head -n 1 n50.txt | tr -d ' ' | tr -d '\n' | wc -c
 
-echo "Numero de columnas:" ; head -n 1 n50.txt | tr -d ' ' | tr -d '\n' | wc -c
+
  
 
